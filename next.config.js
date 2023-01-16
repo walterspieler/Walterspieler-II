a3b5e175279bd9e5d2ee7fc998f836d51dc6708f
Matt Walterspieler <mwalterspieler@gmail.com>
2023-01-16 17:38:40 +0100

    fix(Global): :rocket: Next Export fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const { i18n } = require('./next-i18next.config');
-
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n,
   images: {
     domains: [],
     unoptimized: true,
