66e0d85907daeaddf71e37e295313068d933c1c0
Matt Walterspieler <mwalterspieler@gmail.com>
2023-01-16 16:46:13 +0100

    ci(Global): :construction_worker: Add CI

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 /** @type {import('next').NextConfig} */
 
 const { i18n } = require('./next-i18next.config')
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,7 +10,9 @@ const nextConfig = {
   i18n,
   images: {
     domains: [],
+    unoptimized: true,
   },
+  assetPrefix: isProd ? '/Walterspieler-II/' : '',
 }
 
 module.exports = nextConfig
