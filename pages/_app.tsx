0d200ce1fd6c5dc2ed8ca540f4141971315f10f4
Matt Walterspieler <mwalterspieler@gmail.com>
2023-01-25 15:34:37 +0100

    feat(Content): :mag: Add Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { AppProps } from "next/app";
 import "../styles/global.css";
 import { appWithTranslation } from "next-i18next";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 config.autoAddCss = false;
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <GoogleAnalytics trackPageViews />
+    <Component {...pageProps} />
+  </>
 );
 
 export default appWithTranslation(MyApp);
